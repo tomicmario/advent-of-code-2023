adfbba018651adf79a1c7a3de3e2b5a3c02871a1
Mario Tomic <mario-tomic@hotmail.com>
2023-12-06 21:05:57 +0100

    day 6 part 1

--- src/day/6.clj
+++ src/day/6.clj
@@ -1,1 +1,26 @@
-(ns day.6)
+(ns day.6
+  (:require [clojure.string :as str]))
+
+(defn parse [s]
+  (mapv read-string (-> (second (str/split s #":"))
+                        (str/trim)
+                        (str/split #" +"))))
+
+(defn distances [[time dist]]
+  (loop [held 0
+         acc []]
+    (if (= held time) acc
+        (let [travelled (* held (- time held))
+              updated (if (> travelled dist) (conj acc travelled) acc)]
+          (recur (inc held) updated)))))
+
+(defn solve [s]
+  (let [tokens (str/split-lines (slurp s))
+        time (parse (first tokens))
+        distance (parse (second tokens))
+        data (map #(concat [%1] [%2]) time distance)]
+    (->> (mapv distances data)
+         (mapv count)
+         (reduce *))))
+
+(solve "resources/input.txt")
